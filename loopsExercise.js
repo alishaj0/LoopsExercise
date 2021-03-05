0ec7c68eca37b3e2700c19f2ddf778cbce3d3fa2
Alisha Bolin <alishabolin@KCCCs-MBP.lan>
2021-03-05 13:47:56 -0600

    completed exercise without last bonus

---
+++ loopsExercise.js
@@ -0,0 +1,59 @@
+// LOOPS EXERCISE
+
+//1)
+for (i = 1; i <= 7; i++) {
+    console.log(i);
+}
+
+console.log("==========");
+
+//2)
+for (i = 5; i <= 25; i +=4){
+    console.log(i);
+}
+
+console.log("==========");
+
+//3a)
+const wizards = ["Harry Potter", "Hermione Granger", "Ron Weasley"];
+//3b)
+for (names of wizards) {
+    console.log(names);
+}
+
+console.log("==========");
+
+//4a)
+let harryPotterMovies = 0; 
+//4b)
+while (harryPotterMovies++ <= 7) {
+    console.log(harryPotterMovies);
+}
+
+// while (harryPotterMovies <= 8) {
+//     console.log(harryPotterMovies);
+//     harryPotterMovies++
+// }
+
+console.log("==========");
+
+
+//BONUS
+//5a)
+const hogwartsHouses = `Gryffindor, Hugglepuff, Ravenclaw, Slytherin`;
+//5b)
+for (char of hogwartsHouses){
+    console.log(char);
+}
+
+
+console.log("==========");
+
+
+//6a)
+const quote = [
+    "Yer",
+    "A",
+    "Wizzard",
+    "Harry"
+];
